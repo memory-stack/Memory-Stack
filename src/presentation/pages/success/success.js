2eec0ec102474fab3f0fcc9e260038739b89cf74
Naman Anand <voidmemories@Namans-MacBook-Air.local>
2021-12-14 10:56:38 +0530

    success note added

--- src/presentation/pages/success/success.js
+++ src/presentation/pages/success/success.js
@@ -1,13 +1,16 @@
-import { postRequest } from "../../../data/data-source/remote/apiCall";
-import { SIGNUP } from "../../../data/data-source/remote/apiList";
+import { postRequest } from '../../../data/data-source/remote/apiCall';
+import { SIGNUP } from '../../../data/data-source/remote/apiList';
 
 function Success() {
   return (
     <div className="body">
       <p className="headline">Success.</p>
       <p className="bodyText">
-        A verification link has been sent to your email address. Please{" "}
+        A verification link has been sent to your email address. Please{' '}
         <span>verify to continue.</span>
+      </p>
+      <p style={{ color: '#FF7272', fontSize: '14px' }} className="bodyText">
+        Note: Check your spam folder.
       </p>
     </div>
   );
